9f939dec9326ff42c18688363d1bf6dc13b0eefa
Kinggust83 <49874200+Kinggust83@users.noreply.github.com>
2023-06-06 10:05:28 +0200

    Add files via upload

---
+++ src/Neurone/NeuroneHeavyside.java
@@ -0,0 +1,11 @@
+package Neurone;
+
+
+public class NeuroneHeavyside extends Neurone
+{
+	// Fonction d'activation d'un neurone (peut facilement être modifiée par héritage)
+	protected float activation(final float valeur) {return valeur >= 0 ? 1.f : 0.f;}
+	
+	// Constructeur
+	public NeuroneHeavyside(final int nbEntrees) {super(nbEntrees);}
+}
